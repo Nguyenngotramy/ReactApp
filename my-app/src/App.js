57dc2bfc3149f1268290048de3e9154e2680c863
NguyenNgoTraMy <Nguyenngotramy2427@gmail.com>
2025-06-16 10:00:37 +0700

    create App.js

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyButton from './MyButton';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyButton />
+      </header>
+    </div>
+  );
+}
+export default App;
--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("Đây là file test để kiểm tra Git!");
+import React from 'react';
+import MyButton from './MyButton';
+function App() {
+  return (
+    <div>
+      <h1>Chào React!</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
